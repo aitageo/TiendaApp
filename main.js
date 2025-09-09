8592dea3c2bd84685c4468edadb55406367688e5
aitageo <aitageo1980@hotmail.com>
2025-09-09 07:15:16 -0500

    cambios en main.js

--- main.js
+++ main.js
@@ -9,14 +9,53 @@ document.addEventListener('DOMContentLoaded',()=>{
     
  })
 
- carrito.addEventListener('mouseout', ()=>{
+ carrito.addEventListener('mouseleave', (e)=>{
     const contenedorCarrito = document.querySelector('.mostrar-carrito');
-    contenedorCarrito.style.display = "none";
+    e.preventDefault();
+    setTimeout( ()=>{
+       contenedorCarrito.style.display = "none";
     
- })
- 
+    },1000); 
+ });
 
+const botones = document.querySelectorAll('.btn-primary');
+botones.forEach((boton)=>{
+    boton.addEventListener('click', (e)=>{
+      e.preventDefault();
+      console.log(`Diste click en un boton`);
+      const datos = document.querySelector('.btn-primary').parentNode.parentNode;
+      leerDatos(datos);
+          
+    })
 
+})
+
+function leerDatos(datos){
+    const infoProducto = {
+        imagen: datos.querySelector('img').src,
+        titulo: datos.querySelector('h5').textContent,
+        precio: datos.querySelector('.precio').textContent,
+        id: datos.querySelector('button').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoProducto);
+      insertarCarrito(infoProducto);
+    
+};
+
+function insertarCarrito(producto){
+   const contenedorCarrito = document.querySelector('.mostrar-carrito table tbody');
+    const row = document.createElement('div');
+    row.innerHTML = `
+    <div class="row">
+        <div class="col-4">
+            <img src="${producto.imagen}" width="20%">
+            </div>`;
+      contenedorCarrito.appendChild(row);
+
+}
+
+//fin DOMContentLoaded
 });
 
 
