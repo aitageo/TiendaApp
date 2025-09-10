d7c8559b5ec7a74b1db8af30e3e944305133d768
aitageo <aitageo1980@hotmail.com>
2025-09-09 19:23:02 -0500

    Eliminar funcionando en el carrito

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 //carrito de compras
 
 document.addEventListener('DOMContentLoaded',()=>{
- const carrito = document.getElementById('carrito');
+   let count = 0;
+   const carrito = document.getElementById('carrito');
+   totalCarrito = [];
  carrito.addEventListener('mouseover', ()=>{
     const contenedorCarrito = document.querySelector('.mostrar-carrito');
     contenedorCarrito.style.display = "block";
@@ -20,15 +22,27 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 const botones = document.querySelectorAll('.btn-primary');
 botones.forEach((boton)=>{
-    boton.addEventListener('click', (e)=>{
+   boton.addEventListener('click', (e)=>{
+      const carritocount = document.getElementById('carrito-count');
+      count++;
+      carritocount.textContent = count;
+      setTimeout( ()=>{
+         boton.textContent = "agregando al carrito...";
+      },0);
       e.preventDefault();
       console.log(`Diste click en un boton`);
-      const datos = document.querySelector('.btn-primary').parentNode.parentNode;
+      const datos = e.target.closest('.productos');
       leerDatos(datos);
-          
-    })
-
+      setTimeout( ()=>{
+         boton.textContent = "agregado al carrito";
+      },2000);
+      setTimeout( ()=>{
+         boton.textContent = "agregar al carrito";    
+      },3000)
 })
+
+});
+
 
 function leerDatos(datos){
     const infoProducto = {
@@ -44,16 +58,37 @@ function leerDatos(datos){
 };
 
 function insertarCarrito(producto){
+   const {imagen,titulo,precio,id} = producto;
    const contenedorCarrito = document.querySelector('.mostrar-carrito table tbody');
-    const row = document.createElement('div');
+    const row = document.createElement('tr');
     row.innerHTML = `
-    <div class="row">
-        <div class="col-4">
-            <img src="${producto.imagen}" width="20%">
-            </div>`;
+            <td><img src="${imagen}" class="imagen-carrito"></td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td><a href="#" class="btn btn-danger eliminar" data-id="${id}">X</a></td>`;
       contenedorCarrito.appendChild(row);
-
 }
+
+
+      const contenedorCarrito = document.querySelector('.mostrar-carrito table tbody');
+      contenedorCarrito.addEventListener('click',(e)=>{
+         if(e.target.classList.contains('eliminar')){
+            eliminarProducto(e);
+         }
+      });
+      
+      function eliminarProducto(e){
+         e.preventDefault();
+         const productoId = e.target.getAttribute('data-id');
+         console.log(productoId);
+         e.target.parentElement.parentElement.remove();
+         count--;
+         const carritocount = document.getElementById('carrito-count');
+         carritocount.textContent = count;
+      }
+
+
+
 
 //fin DOMContentLoaded
 });
